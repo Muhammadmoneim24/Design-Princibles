8f8deaffb6ef79927155b83de445a63147e323c4
Mohamed.A.Abdelmoneim <muhammadmoneim27@gmail.com>
2024-02-29 16:12:08 +0200

    new commit

---
+++ EncabsulateWhatVaries/EncabsulateWhatVaries/PizzaConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncabsulateWhatVaries
+{
+    public class PizzaConstants
+    {
+        public const string CheesePizza = "cheese";
+        public const string ChickenPizza = "chicken";
+        public const string VegeterianPizza = "vegeterian";
+
+    }
+}
---
+++ EncabsulateWhatVaries/EncabsulateWhatVaries/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace EncabsulateWhatVaries
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza pizza = Pizza.Order(PizzaConstants.VegeterianPizza);
+            Console.WriteLine(pizza);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Pizza 
+    {
+        public virtual string Title => $"{nameof(Pizza)}";
+        public virtual decimal Price => 10m;
+
+        private static Pizza Create(string type)
+        {
+            Pizza pizza;
+
+            if (type.Equals(PizzaConstants.CheesePizza))
+                pizza = new Cheese();
+
+            else if (type.Equals(PizzaConstants.VegeterianPizza))
+                pizza = new Vegeterian();
+
+            else
+                pizza = new Chicken();
+
+            return pizza;
+        }
+        public static Pizza Order(string type) 
+        {
+            Pizza pizza = Create(type);
+
+            Prepare();
+            Cook();
+            Cut();
+
+           return pizza;
+        }
+
+        private static void Prepare() 
+        {
+            Console.Write("Preparing....");
+            Thread.Sleep(3000);
+            Console.WriteLine("Completed");
+        }
+
+        private static void Cook()
+        {
+            Console.Write("Cooking....");
+            Thread.Sleep(3000);
+            Console.WriteLine("Completed");
+        }
+
+        private static void Cut()
+        {
+            Console.Write("Cutting and boxing....");
+            Thread.Sleep(3000);
+            Console.WriteLine("Completed");
+        }
+
+        public override string ToString()
+        {
+            return $"\n{Title}"+
+                   $"\n\tPrice: {Price.ToString("C")}";
+        }
+
+
+    }
+
+    class Cheese : Pizza 
+    {
+        public override string Title => $"{base.Title} {nameof(Cheese)}";
+        public override decimal Price => base.Price + 3m;
+    }
+
+    class Chicken : Pizza
+    {
+        public override string Title => $"{base.Title} {nameof(Chicken)}";
+        public override decimal Price => base.Price + 6m;
+    }
+
+    class Vegeterian : Pizza
+    {
+        public override string Title => $"{base.Title} {nameof(Vegeterian)}";
+        public override decimal Price => base.Price + 4m;
+    }
+
+}
